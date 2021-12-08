c366f04ef4c9e0028a330b6606a1a3627244ccbb
tyler komoroske <tjkomor@clemson.edu>
2021-12-08 16:15:08 -0700

    able to build and run tests with forge. sweeeettttttt

--- src/Contract.sol
+++ src/Contract.sol
@@ -2,3 +2,14 @@
 pragma solidity 0.8.10;
 
 contract Contract {}
+
+contract Foo {
+    uint256 public x = 1;
+    function set(uint256 _x) external {
+        x = _x;
+    }
+
+    function double() external {
+        x = 2 * x;
+    }
+}
--- src/test/Contract.t.sol
+++ src/test/Contract.t.sol
@@ -1,12 +1,27 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity 0.8.10;
 
-import "ds-test/test.sol";
+import "src/Contract.sol";
 
-contract ContractTest is DSTest {
-    function setUp() public {}
 
-    function testExample() public {
-        assertTrue(true);
+contract FooTest {
+    Foo foo;
+
+    // The state of the contract gets reset before each
+    // test is run, with the `setUp()` function being called
+    // each time after deployment.
+    function setUp() public {
+        foo = new Foo();
+    }
+
+    // A simple unit test
+    function testDouble() public {
+        require(foo.x() == 1);
+        foo.double();
+        require(foo.x() == 2);
+    }
+
+    function testSomething() public {
+        require(1 == 1);
     }
 }
